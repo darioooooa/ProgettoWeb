9a54c2b1a559c072064bdd68988d4efa77b0e3bb
darioooooa <dario2004dp@gmail.com>
2025-11-24 17:45:14 +0100

    Aggiunta pagina login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home'; // Importa la classe dal tuo file
 import {Register} from './register/register'
-
+import {Login} from './login/login'
 export const routes: Routes = [
   { path: '', component: Home },
-  { path:'register', component: Register}
+  { path:'register', component: Register},
+   {path: 'login', component:Login}
+
 ];
