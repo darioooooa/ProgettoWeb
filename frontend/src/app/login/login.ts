fe9215e63ab8041c0feea9a2f77f84a4c95e2f94
pierpasquale <pierpasqualegulluscio04@gmail.com>
2025-11-24 18:12:53 +0100

    login

--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+    imports: [
+        RouterLink,FormsModule,RouterModule
+    ],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  dati={
+    email:'',
+    password:''
+
+  }
 
 }
